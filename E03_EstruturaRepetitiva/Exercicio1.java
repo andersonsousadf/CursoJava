c32e69ff045b84fba8df2ad86a382d4f3f842a25
Anderson <andersonsousadf@gmail.com>
2020-06-16 22:57:54 -0300

    c02 - Exercícios resolvidos Estrutura Repetitiva While

---
+++ E03_EstruturaRepetitiva/Exercicio1.java
@@ -0,0 +1,28 @@
+/*Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha
+incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser
+impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.*/
+package E03_EstruturaRepetitiva;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int senha = 2002;
+        
+        int teste;
+        System.out.println("Digite a senha:");
+        teste = sc.nextInt();
+        
+        while (teste != senha){
+            System.out.println("Senha Inválida!");
+            System.out.println("Digite novamente:");
+            teste = sc.nextInt();
+            
+        }
+        System.out.println("ACESSO PERMITIDO.");
+       
+        sc.close();
+    }
+ 
+}
